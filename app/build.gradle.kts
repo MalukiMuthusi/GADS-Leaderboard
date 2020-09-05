07bb20001080ba2776ea241578654b764a9fb103
Maluki Muthusi <malukimuthusi@yahoo.com>
2020-09-05 22:51:20 +0300

    feat: Add viewPager2, and tablayout.     #docs: add bewell convention to readme
        #Couple of typos fixed:
        #- indentation
        #- syntax highlighting
        #- start periodic checking
        #- missing brace
    
        #BREAKING CHANGE: isolate scope bindings definition has changed and
        #    the inject option for the directive controller injection was removed.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,6 +8,10 @@ android {
     compileSdkVersion(30)
     buildToolsVersion("30.0.2")
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "codes.malukimuthusi.gadsleaderboard"
         minSdkVersion(23)
@@ -44,8 +48,9 @@ dependencies {
     implementation("androidx.core:core-ktx:1.3.1")
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.1")
+    implementation("com.google.android.material:material:1.2.0")
     testImplementation("junit:junit:4.13")
     androidTestImplementation("androidx.test.ext:junit:1.1.2")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
-
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
 }
